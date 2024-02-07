f3496ce2f50b9b8fc67aa6a4e07f0627942541f6
ernestchsheu <ErnestCHSheu@fih-foxconn.com>
2024-02-07 18:36:40 +0800

    Updated

--- bundled/tool-libs/meow_code_formatter/versions.py
+++ bundled/tool-libs/meow_code_formatter/versions.py
@@ -9,7 +9,7 @@ __description__ = 'Formatter for Python code'
 __author__ = "Ernest Sheu"
 __author_email__ = "zomsheu@gmail.com"
 __status__ = "Develop"  # Stable, Develop
-__version__ = '0.0.2'
+__version__ = '0.0.1'
 __date__ = "07 Feb 2024"
 
 # for cython complier parameters
